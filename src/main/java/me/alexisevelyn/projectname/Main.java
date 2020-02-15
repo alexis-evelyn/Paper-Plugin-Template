2a6c72a66613e1c729bb8b5a129321115b1d2dad
Alex Evelyn <gpg@senorcontento.com>
2020-02-15 00:21:36 -0500

    Added Template For Paper Plugin Projects

---
+++ src/main/java/me/alexisevelyn/projectname/Main.java
@@ -0,0 +1,76 @@
+package me.alexisevelyn.projectname;
+
+// Bukkit Imports
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	private boolean debug = true;
+
+	// Getters and Setters
+
+	public boolean isDebug() {
+		return this.debug;
+	}
+
+	public boolean isVerboseEnough(Verbosity superverbose) {
+		// TODO: Have functions called this with a requested verbosity and
+		// return true if verbosity is that value or higher
+
+
+		return isDebug();
+	}
+
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+	}
+
+	// Event Listeners
+
+	@Override
+    public void onEnable() {
+		// TODO: Hook Into GriefPrevention and Worldguard
+		// ...
+
+		// Register Bukkit Listeners (For Event Handlers)
+		Bukkit.getPluginManager().registerEvents(this, this);
+
+		// Register Commands
+		this.getCommand("commandOne").setExecutor(new Commands());
+		this.getCommand("commandTwo").setExecutor(new Commands());
+
+		// Announce Successful Start
+		getLogger().info("Project Name has successfully started!!!");
+	}
+
+	@Override
+    public void onDisable() {
+		getLogger().info("Thank you for using Project Name!!!");
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
+	public void onChunkLoad(ChunkLoadEvent event) {
+		if(isVerboseEnough(Verbosity.superverbose)) {
+			// getLogger().info(ChatColor.BLUE + "<onChunkLoad> New Chunk: " + event.isNewChunk());
+			// getLogger().info(ChatColor.BLUE + "<onChunkLoad> Asynchronous: " + event.isAsynchronous());
+			//
+			// getLogger().info(ChatColor.BLUE + "<onChunkLoad> World: " + event.getWorld());
+		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
+	public void onChunkUnload(ChunkUnloadEvent event) {
+		if(isVerboseEnough(Verbosity.superverbose)) {
+			// getLogger().info(ChatColor.GREEN + "<onChunkUnload> Is Saving Chunk: " + event.isSaveChunk());
+			// getLogger().info(ChatColor.GREEN + "<onChunkUnload> Asynchronous: " + event.isAsynchronous());
+			//
+			// getLogger().info(ChatColor.GREEN + "<onChunkUnload> World: " + event.getWorld());
+		}
+	}
+}
